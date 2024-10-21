04df4b6dbc13fb65bf638211bc2df77c2431ccf0
Katherina314 <ekasmile714@gmail.com>
2024-10-21 22:24:55 +0300

    Merge remote-tracking branch 'origin/main'

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ group = "io.hexlet"
 
 version = "1.0-SNAPSHOT"
 
-application { mainClass.set(/* value = */ "hexlet.code.App") }
+application { mainClass.set("hexlet.code.App") }
 
 repositories { mavenCentral() }
 
@@ -37,4 +37,4 @@ tasks.test {
     }
 }
 
-tasks.jacocoTestReport { reports { xml.required.set(true) } }/*
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
