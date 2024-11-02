2dc97378dcc77a1995a697823c555b5dce206e1b
Katherina314 <ekasmile714@gmail.com>
2024-11-02 23:25:00 +0300

    Merge remote-tracking branch 'origin/main'

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,17 +1,41 @@
 package hexlet.code;
 
-import hexlet.code.Cli;
 import java.util.Scanner;
+import hexlet.code.Cli;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner();
-        System.out.println("Please enter the game number and press Enter");
-        System.out.println("1 - Greet");
-        System.out.println("0 - Exit");
-    }
+        Scanner scanner = new Scanner(System.in);
 
-}
+        String command;
+
+        do {
+            System.out.println("Please enter the game number and press Enter");
+            System.out.println("1 - Greet");
+            System.out.println("0 - Exit");
+
+            command = scanner.nextLine();
+
+            switch (command) {
+                case "1":
+                    System.out.println("Welcome to the Brain Games!\n" +
+                            "May I have your name?");
+                    command = scanner.nextLine();
+                    System.out.println(command);
+                    break;
+
+                case "0":
+                    System.out.println("Goodbuy!");
+                    break;
+            }
+            ;
+
+        }while (!command.equals(""));
+
+
+
+    };
+};
 
 
 
