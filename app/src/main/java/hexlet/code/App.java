841050bec64ab0a9b3475e111f57db2da2c619b0
Katherina314 <ekasmile714@gmail.com>
2024-11-15 22:51:17 +0300

    Merge remote-tracking branch 'origin/main'

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,13 +1,13 @@
 package hexlet.code;
 
 import java.util.Scanner;
-import hexlet.code.Even;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
+        System.out.println("3 - Calc");
         System.out.println("0 - Exit");
 
         System.out.println("Your choice: ");
@@ -22,6 +22,10 @@ public class App {
 
                 case "2":
                     Even.run();
+                    break;
+
+                case "3":
+                    Calculator.run();
                     break;
 
                 case "0":
---
+++ app/src/main/java/hexlet/code/Calculator.java
@@ -0,0 +1,44 @@
+package hexlet.code;
+
+public class Calculator {
+    static final int MAX_OPERAND_VALUE = 100;
+    static final String GAME_RULE = "count these numbers";
+    static final String[] OPERATIONS = {"+", "-", "*"};
+
+    public static void run() {
+        String[][] questionsAnswers = new String[Engine.MAX_ROUNDS_OF_GAME][Engine.QA_FOR_ONE_ROUND];
+
+        for (var i = 0; i < Engine.MAX_ROUNDS_OF_GAME; i++) {
+            questionsAnswers[i] = generateRoundData();
+        }
+
+        Engine.playGame(questionsAnswers, GAME_RULE);
+    }
+
+    static String[] generateRoundData() {
+        String[] round = new String[Engine.QA_FOR_ONE_ROUND];
+
+        var firstNumber = Utils.randomNumber(MAX_OPERAND_VALUE);
+        var secondNumber = Utils.randomNumber(MAX_OPERAND_VALUE);
+        var operator = Utils.randomNumber(OPERATIONS.length);
+
+        var question = firstNumber + " " + OPERATIONS[operator] + " " + secondNumber;
+        var answer = calculate(firstNumber, secondNumber, OPERATIONS[operator]);
+
+        round[Engine.QUESTION_INDEX] = question;
+        round[Engine.ANSWER_INDEX] = String.valueOf(answer);
+
+        return round;
+    }
+
+    static int calculate(int firstNumber, int secondNumber, String operation) {
+        return switch (operation) {
+            case "+" -> firstNumber + secondNumber;
+            case "-" -> firstNumber - secondNumber;
+            case "*" -> firstNumber * secondNumber;
+            default -> throw new RuntimeException("Unknown operation: " + operation);
+        };
+    }
+
+
+}
