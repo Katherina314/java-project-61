8043f85858e27ac6d59298e7389d62263464fa17
Katherina314 <ekasmile714@gmail.com>
2024-11-28 22:42:42 +0300

    Merge remote-tracking branch 'origin/main'

--- app/settings.gradle.kts
+++ app/settings.gradle.kts
@@ -1,3 +1,10 @@
 rootProject.name = "app"
 include("src:main:java:hexlet:games")
 findProject(":src:main:java:hexlet:games")?.name = "games"
+include("src:main:java:games")
+findProject(":src:main:java:games")?.name = "games"
+include("src:main:java:untitled")
+findProject(":src:main:java:untitled")?.name = "untitled"
+include("src:main:java:untitled")
+findProject(":src:main:java:untitled")?.name = "untitled"
+include("untitled1")
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,5 @@
 package hexlet.code;
 
-import hexlet.code.Calculator;
-import hexlet.code.Even;
-import hexlet.code.GCD;
-import hexlet.code.Prime;
-import hexlet.code.Progression;
 import java.util.Scanner;
 
 public class App {
